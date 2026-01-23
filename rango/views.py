c3c9b2891f0c6b4f844a01a15ebdbc037e65c603
“Isaac <2961656W@student.gla.ac.uk>
2026-01-23 11:30:08 +0000

    Chapter 3 exercises completed

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("""Rango says hello world!<br><a href="about">About</a>""")
+
+
+def about(request):
+    
+    response = """Rango Says: Here is the about page.<br>
+                        <a href="/rango">Index</a>"""
+    
+    return HttpResponse(response)
