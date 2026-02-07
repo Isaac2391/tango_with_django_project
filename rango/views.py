37db8fc47e9ef04cbc798f04567a9eb01ecd61c0
“Isaac <2961656W@student.gla.ac.uk>
2026-02-07 22:08:41 +0000

    Chapter 9 exercises done

--- rango/views.py
+++ rango/views.py
@@ -61,10 +61,11 @@ def show_category(request,category_name_slug):
         
     return render(request,'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
-    
+        
     form = CategoryForm() 
-    
+        
     if request.method == 'POST':
         form = CategoryForm(request.POST)
         
@@ -76,9 +77,10 @@ def add_category(request):
         
         else:
             print(form.errors)
-            
+                            
     return render(request,'rango/add_category.html', {'form':form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -175,7 +177,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
 
 @login_required 
 def user_logout(request):
