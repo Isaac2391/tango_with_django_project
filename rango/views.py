6d91251a0f9d540390afc18ae966f3018749eef1
“Isaac <2961656W@student.gla.ac.uk>
2026-02-10 16:28:10 +0000

    Finished chapter 10

--- rango/views.py
+++ rango/views.py
@@ -16,11 +16,9 @@ from rango.forms import CategoryForm
 from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
 
+from datetime import datetime
+
 def index(request):
-# Order the categories by the number of likes in descending order.
-# Retrieve the top 5 only -- or all if less than 5.
-# Place the list in our context_dict dictionary (with our boldmessage!)
-# that will be passed to the template engine.
 
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
@@ -30,8 +28,12 @@ def index(request):
     context_dict['categories'] = category_list 
     context_dict['pages'] = page_list
     
-    # Render the response and send it back!
-    return render(request,'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    
+    response = render(request,'rango/index.html',context=context_dict)
+
+    return response 
+    
 
 def about(request):
     
@@ -39,7 +41,17 @@ def about(request):
     print(request.method)
     # prints out the user name, if no one is logged in it prints `AnonymousUser`
     print(request.user)
-    return render(request, 'rango/about.html', {})
+    
+    visitor_cookie_handler(request)
+
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+        
+    context_dict = {} 
+    context_dict['visits'] = request.session['visits']
+        
+    return render(request, 'rango/about.html', context=context_dict)
 
 
 def show_category(request,category_name_slug):
@@ -128,7 +140,7 @@ def register(request):
             profile = profile_form.save(commit=False)
             profile.user = user 
             
-            if 'picutre' in request.FILES:
+            if 'picture' in request.FILES:
                 profile.picture = request.FILES['picture']
                 
             profile.save() 
@@ -185,4 +197,27 @@ def user_logout(request):
     logout(request)
     
     return redirect(reverse('rango:index'))
+
+def visitor_cookie_handler(request):
+    
+    visits = int(request.session.get('visits', '1'))
+    
+    last_visit_cookie = request.session.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie 
+        
+    request.session['visits'] = visits
+
+def get_server_side_cookie(request,cookie,default_val=None):
+    
+    val = request.session.get(cookie)
+    
+    if not val:
+        val = default_val
+    return val 
 
